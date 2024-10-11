9b059e71ffb188e4481105e04c57235c9cc84b14
iamcarlogg <00052220@uca.edu.sv>
2024-10-11 15:42:03 -0600

    agregando colores al tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      primary: '#4F8B6F',
+      secondary: '#48735F',
+      highlight: '#BFBFBF',
+      shadow: '#404040',
+      black: '#0D0D0D',
+      buttons: '#9DDAC9',
+      panel_color:'#F2F4D4',
+    },
   },
   plugins: [],
 }
